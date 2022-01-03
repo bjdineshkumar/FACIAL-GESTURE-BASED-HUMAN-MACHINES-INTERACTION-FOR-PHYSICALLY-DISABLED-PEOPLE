167cb41251c70d180d4ce0c5b1bc2e2431a9b2f9
bjdineshkumar <dn466091@dal.ca>
2022-01-03 19:37:56 -0400

    Added the project files

---
+++ main_demo.py
@@ -0,0 +1,39 @@
+import cv2
+from gaze_tracking import GazeTracking
+from page import Page
+from sklearn.svm import SVC
+import pandas as pd
+import numpy as np
+
+gaze = GazeTracking()
+webcam = cv2.VideoCapture(0)
+outn="init.avi" 
+
+fps = webcam.get(cv2.CAP_PROP_FPS)
+print(fps)
+_, frame = webcam.read()
+(hgt, wid, dep) = frame.shape
+frc = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
+out = cv2.VideoWriter(outn, frc, 25.0, (wid*2, hgt), 1)
+
+
+page_initialization = Page(-1, gaze, webcam, out)
+gaze.set_clf(page_initialization.initialization())
+
+pages = [Page(0, gaze, webcam,out), Page(1, gaze, webcam,out), Page(2, gaze, webcam,out), Page(3, gaze, webcam,out),
+         Page(4, gaze, webcam,out), Page(5, gaze, webcam,out), Page(6, gaze, webcam,out), Page(7, gaze, webcam,out),
+         Page(8, gaze, webcam,out), Page(9, gaze, webcam,out), Page(10, gaze, webcam,out)]
+index = 0
+str_input = ""
+while True:
+    index, str_input, whe_break = pages[index].run(str_input, out)
+
+    if(whe_break):
+        break
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+print(str_input)
+webcam.release()
+out.release()
+print(hgt, wid)
